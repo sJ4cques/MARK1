c983232647e567ce4c55a7123f3acedafa375f5f
sJ4cques <137961590+sJ4cques@users.noreply.github.com>
2023-08-15 17:22:25 -0600

    15/08/23

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,21 @@
-import { createNativeStackNavigator } from "@react-navigation/native"
-import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from './screens/Home'
+import Add from './screens/Add';
 
-import Home from "./screens/Home"
-import Add from "./screens/Add"
-
-const Stack = createNativeStackNavigator()
+const Stack = createStackNavigator();
 
 function MyStack() {
     return(
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Add" component={Add} />
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Add" component={Add} options={{presentation: 'modal'}}/>
         </Stack.Navigator>
     )
 }
 
-export default function Navigation(){
-    return(
+export default function Navigation() {
+    return (
         <NavigationContainer>
             <MyStack />
         </NavigationContainer>
