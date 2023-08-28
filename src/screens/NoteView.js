0b6fd3166970ea5a830490f972a205a70d169fed
sJ4cques <137961590+sJ4cques@users.noreply.github.com>
2023-08-27 18:44:47 -0600

    27/08/23

--- src/screens/NoteView.js
+++ src/screens/NoteView.js
@@ -1,31 +1,11 @@
-import * as React from "react";
-import * as RN from "react-native";
+import * as React from "react"
+import * as RN from "react-native"
 import { getFirestore, doc, getDoc, onSnapshot, orderBy, query } from "firebase/firestore"
-import { database } from "../config/fb";
+import { database } from "../config/fb"
 
 
 export default function NoteView () {
     
-
-    React.useEffect(() => {
-        const collectionRef = collection(database, "products");
-        const q = query(collectionRef, orderBy("createdAt", "desc"));
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-         
-          console.log("querySnapshot unsusbscribe");
-          setProducts(
-            querySnapshot.docs.map((doc) => ({
-              id: doc.id,
-              name: doc.data().name,
-              price: doc.data().price,
-              isSold: doc.data().isSold,
-              createdAt: doc.data().createdAt,
-              link: doc.data().link
-            }))
-          );
-        });
-        return unsubscribe; 
-      }, []);
 
     
     
@@ -34,7 +14,7 @@ export default function NoteView () {
             <RN.Text style={styles.name}>TITULO</RN.Text>
             <RN.Text style={styles.price}>DESCRIPCIÓN</RN.Text>
 
-            <RN.Button title="Información" onPress={Info} /> 
+            <RN.Button title="Información" /> 
         </RN.View>
     )
 
@@ -45,15 +25,15 @@ const styles = RN.StyleSheet.create({
         padding: 16,
         backgroundColor: '#fff',
         margin: 16,
-        borderRadius: 8,
+        borderRadius: 8
     },
     name: {
         fontSize: 32,
-        fontWeight: 'bold',
+        fontWeight: 'bold'
     },
     price: {
         fontSize: 24,
         fontWeight: 'bold',
-        color: 'gray',
+        color: 'gray'
     }
 })
